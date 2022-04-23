a85d820f5523cbad5b690f879f415856393cb962
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2022-04-23 18:51:40 +1000

    OK MOVEMENTS MADE

--- Lunch Time Rush/Assets/Code/MovementSystems/DelMove.cs
+++ Lunch Time Rush/Assets/Code/MovementSystems/DelMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class DelMove : MonoBehaviour
 {
     private GameManager gameManager;
     private PlayerInput input;
@@ -12,8 +12,6 @@ public class PlayerController : MonoBehaviour
     private Vector2 direction;
     public float changeReq = 5;
     private bool touching;
-    public Image joystick;
-
 
     // Start is called before the first frame update
     void Start()
@@ -32,32 +30,14 @@ public class PlayerController : MonoBehaviour
         //Debug.Log(movement);
         //if (input.actions["Held"].ReadValue<float>() > 0)
         if (input.actions["PrimaryContact"].ReadValue<float>() == 1)
-        {
-            /*
-            Vector2 pos = input.actions["PrimaryPosition"].ReadValue<Vector2>();
-            //Vector2 del = input.actions["Delta"].ReadValue<Vector2>();
-            Vector2 startPos = input.actions["StartPosition"].ReadValue<Vector2>();
-            Vector3 worldStartPos = Camera.main.ScreenToWorldPoint(startPos);
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
-            worldPos.z = 0;
-            //Debug.Log(worldPos + " | " + transform.position + " | "  +
-            //    worldStartPos);
-            //transform.position = worldPos;
-            direction = (worldPos - worldStartPos);
-            
-            //direction = Vector3.ClampMagnitude(direction, 1);
-            //Debug.Log(direction.magnitude);
-            transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
-            */
+        {            
             if (input.actions["Hold"].triggered)
             {
                 touching = true;
             }
             if (touching)
             {
-                
                 Vector2 pos = input.actions["PrimaryPosition"].ReadValue<Vector2>();
-                
                 Vector2 startPos = input.actions["StartPosition"].ReadValue<Vector2>();
                 Vector3 worldStartPos = Camera.main.ScreenToWorldPoint(startPos);
                 Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
@@ -65,20 +45,17 @@ public class PlayerController : MonoBehaviour
                 worldStartPos.z = 0;
 
                 direction = (worldPos - worldStartPos);
+
+                //transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
                 direction = Vector3.ClampMagnitude(direction, 1);
                 transform.Translate(direction * moveSpeed * Time.deltaTime);
                 Debug.Log(direction.normalized.magnitude);
-                
-                joystick.transform.position = worldStartPos;
-                joystick.enabled = true;
             }
-
         }
         else
         {
             //direction = Vector2.zero;
             touching = false;
-            joystick.enabled = false;
         }
 
 
@@ -87,29 +64,29 @@ public class PlayerController : MonoBehaviour
 
         //if (input.actions["PrimaryContact"].ReadValue<float>() > 0)
         //{
-            /*
-            if (input.actions["Delta"].ReadValue<Vector2>().magnitude 
-                > changeReq)
-            {
-                direction = input.actions["Delta"].ReadValue<Vector2>();
-                
-                direction = Vector2.ClampMagnitude(direction, 1);
-                              
-            }
+        /*
+        if (input.actions["Delta"].ReadValue<Vector2>().magnitude 
+            > changeReq)
+        {
+            direction = input.actions["Delta"].ReadValue<Vector2>();
 
-            transform.Translate(direction * moveSpeed * Time.deltaTime);
-            Debug.Log(direction.magnitude);
-            */
-            /*
-            if (input.actions["CurrentPosition"].ReadValue<Vector2>().magnitude !=
-                input.actions["StartPosition"].ReadValue<Vector2>().magnitude)
-            {
-                float dir = Vector2.Distance(input.actions["CurrentPosition"].ReadValue<Vector2>(),
-                    input.actions["StartPosition"].ReadValue<Vector2>());
-            }
-            */
+            direction = Vector2.ClampMagnitude(direction, 1);
+
+        }
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        Debug.Log(direction.magnitude);
+        */
+        /*
+        if (input.actions["CurrentPosition"].ReadValue<Vector2>().magnitude !=
+            input.actions["StartPosition"].ReadValue<Vector2>().magnitude)
+        {
+            float dir = Vector2.Distance(input.actions["CurrentPosition"].ReadValue<Vector2>(),
+                input.actions["StartPosition"].ReadValue<Vector2>());
+        }
+        */
         //}
-        
+
         /*
         if (input.actions["PrimaryContact"].triggered)
         {
